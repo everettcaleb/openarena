4eb103592e0457fcd15698a1b530f909b69379c8
cheapy <cheapy@f22aa546-3616-0410-a18f-8d81e1de639b>
2006-07-30 18:27:20 +0000

    Adding in the content sources.
    git-svn-id: http://openarena.ws/svn@6 f22aa546-3616-0410-a18f-8d81e1de639b

---
+++ source/assets/models/newrock/fixpath.bat
@@ -0,0 +1,4 @@
+modeltool --renametextures "D:\projects\oa\newrock\initialShadingGroup" "models/weapons2/rocketl/skin" *.md3
+
+modeltool --renametextures "D:\projects\oa\newrock\Plane.001" "rocketFlash" *.md3
+
