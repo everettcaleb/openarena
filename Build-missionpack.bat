ace71222352ba1799bd9ec20048f737f4b29f9fa
cheapy <cheapy@f22aa546-3616-0410-a18f-8d81e1de639b>
2007-07-02 19:14:29 +0000

    ..\zip works
    git-svn-id: http://openarena.ws/svn@475 f22aa546-3616-0410-a18f-8d81e1de639b

--- Build-missionpack.bat
+++ Build-missionpack.bat
@@ -1,4 +1,4 @@
 cd missionpack
-zip -9r ../mp-pak0.pk3 ../CREDITS
-zip -9r ../mp-pak0.pk3 ../COPYING
-zip -9r ../mp-pak0.pk3 *.* -x *svn*
+..\zip -9r ../mp-pak0.pk3 ../CREDITS
+..\zip -9r ../mp-pak0.pk3 ../COPYING
+..\zip -9r ../mp-pak0.pk3 *.* -x *svn*
--- Build-mods.bat
+++ Build-mods.bat
@@ -5,4 +5,4 @@ zip -9r oax-pak0.pk3 COPYING
 
 cd mods
 
-zip -9r ../oax-pak0.pk3 * -x \*svn* -x *.exe
+..\zip -9r ../oax-pak0.pk3 * -x \*svn* -x *.exe
