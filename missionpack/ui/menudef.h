feb935590c95e8aa28ce7c1b164b3561a47fe3ed
cheapy <cheapy@f22aa546-3616-0410-a18f-8d81e1de639b>
2007-07-11 17:29:06 +0000

    starting to make some menu elements common and also menu strings for easier localization
    git-svn-id: http://openarena.ws/svn@556 f22aa546-3616-0410-a18f-8d81e1de639b

--- missionpack/ui/menudef.h
+++ missionpack/ui/menudef.h
@@ -306,3 +306,18 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define VOICECHAT_DEATHINSULT		"death_insult"			// you just killed me
 #define VOICECHAT_KILLGAUNTLET		"kill_gauntlet"			// I just killed you with the gauntlet
 #define VOICECHAT_PRAISE			"praise"				// you did something good
+
+#include "ui/lang_english.h"
+
+#define MP_BACKGROUND "ui/assets/splash.jpg" // background of the menu
+#define MP_SND_SELECT "ui/assets/select.wav" // confirm noise
+#define MP_SND_HOVER "ui/assets/hover.wav" // hover noise
+
+#define MP_TITLECOLOR 1 1 0.6 1 // Title colors
+#define MP_TEXTCOLOR 0.935 0.70 0.24 1 // regular color of menu button text
+#define MP_FOCUSCOLOR 1 0 1 1 1 // regular color of menu button text
+
+#define MP_BOX_BORDER 0.25 0.25 0.25 1
+#define MP_BOX_FORE 1   0.5   0.01   1
+#define MP_BOX_BACK 0.0 0.0 0.0 1
+#define MP_BOX_OUTLINE 1   0.5   0.01   0.3
