4574d15b402e44470a35ba5cfd5feb9eea8dfec6
cheapy <cheapy@f22aa546-3616-0410-a18f-8d81e1de639b>
2007-07-29 16:55:47 +0000

    0.7.1 patch build script. Hope i'm not forgetting anything. (should be sh'able still)
    git-svn-id: http://openarena.ws/svn@578 f22aa546-3616-0410-a18f-8d81e1de639b

---
+++ Build-patch-071.bat
@@ -0,0 +1,29 @@
+zip -9r pak7-patch.pk3 CHANGES
+zip -9r pak7-patch.pk3 COPYING
+
+zip -9r pak7-patch.pk3 scripts/arenas.txt
+zip -9r pak7-patch.pk3 scripts/common.shader
+zip -9r pak7-patch.pk3 scripts/iconsprites.shader
+zip -9r pak7-patch.pk3 scripts/oaflares.shader
+
+zip -9r pak7-patch.pk3 icons/envirosuit.tga
+zip -9r pak7-patch.pk3 icons/flight.tga
+zip -9r pak7-patch.pk3 icons/haste.tga
+zip -9r pak7-patch.pk3 icons/invis.tga
+zip -9r pak7-patch.pk3 icons/medkit.tga
+zip -9r pak7-patch.pk3 icons/noammo.tga
+zip -9r pak7-patch.pk3 icons/quad.tga
+zip -9r pak7-patch.pk3 icons/regen.tga
+zip -9r pak7-patch.pk3 icons/teleporter.tga
+
+zip -9r pak7-patch.pk3 models/players/hnt/drkclothes_blue.tga
+
+zip -9r pak7-patch.pk3 botfiles/rnd.c
+zip -9r pak7-patch.pk3 botfiles/rchat.c
+
+zip -9r pak7-patch.pk3 maps/suspended.bsp
+zip -9r pak7-patch.pk3 maps/suspended.aas
+zip -9r pak7-patch.pk3 maps/oa_bases3.bsp
+zip -9r pak7-patch.pk3 maps/oa_bases3.aas
+zip -9r pak7-patch.pk3 maps/fan.bsp
+zip -9r pak7-patch.pk3 maps/fan.aas
