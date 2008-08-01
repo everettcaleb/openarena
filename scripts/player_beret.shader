50ef3acdf3d3181b60e989f5d184cdec1faffd2e
leilei <leilei@f22aa546-3616-0410-a18f-8d81e1de639b>
2008-08-01 10:30:57 +0000

    - Chaingun spin sound improvement!
    git-svn-id: http://openarena.ws/svn@847 f22aa546-3616-0410-a18f-8d81e1de639b

---
+++ scripts/player_beret.shader
@@ -0,0 +1,9 @@
+models/players/beret/cooldude
+{
+
+	{
+		map models/players/beret/cooldude.tga
+		rgbGen lightingDiffuse
+		tcGen environment
+	}
+}
--- source/assets/models/newplas/fixpath.bat
+++
@@ -1,5 +0,0 @@
-modeltool --renametextures "D:\projects\oa\newplas\Plane" "models/weapons2/plasma/skin" *.md3
-
-modeltool --renametextures "D:\projects\oa\newplas\Plane.001" "models/weapons2/plasma/skin" *.md3
-
-modeltool --renametextures "skin.001" "skin" *.md3
