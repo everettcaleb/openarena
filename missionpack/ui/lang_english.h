2d44d868415fa17868969982a6edd710b5ab62af
leilei <leilei@f22aa546-3616-0410-a18f-8d81e1de639b>
2008-08-04 09:54:05 +0000

    proximity bomb goes to the missionpack not baseoa also quick and dity skin for that commented out not-working sections of the ingame menu and warned about brokenness
    git-svn-id: http://openarena.ws/svn@855 f22aa546-3616-0410-a18f-8d81e1de639b

--- missionpack/ui/lang_english.h
+++ missionpack/ui/lang_english.h
@@ -20,4 +20,4 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#define MISSIONPACK_TITLE			"MissingNoPack"
+#define MISSIONPACK_TITLE			"The Mission Pack"
